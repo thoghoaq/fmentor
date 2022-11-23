fd115b3f4f2163efa54bc31429a59aee1e9645eb
learning2html <honganhle0a@gmail.com>
2022-11-23 14:50:48 +0700

    fix get started screen

--- lib/pages/get_started.dart
+++ lib/pages/get_started.dart
@@ -20,54 +20,62 @@ class _GetStartedState extends State<GetStarted> {
       body: Center(
         child: Stack(
           children: [
-            // Padding(
-            //   padding: const EdgeInsets.all(10.0),
-            //   child: SvgPicture.asset(
-            //     'assets/images/background.svg',
-            //     width: MediaQuery.of(context).size.width,
-            //   ),
-            // ),
-            Center(
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SvgPicture.asset(
+                'assets/images/background.svg',
+                width: MediaQuery.of(context).size.width,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: SvgPicture.asset(
+                'assets/images/four_bubble.svg',
+                width: MediaQuery.of(context).size.width / 1.2,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
               child: Container(
+                height: 400,
                 color: Colors.white,
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: SizedBox(
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              top: 40.0, bottom: 40.0, right: 70.0, left: 70.0),
-                          child: Text(
-                            'Discover your strengths with your mentor',
-                            style: AppFonts.medium(24),
-                            textAlign: TextAlign.center,
-                          ),
+                child: SizedBox(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 40.0, bottom: 40.0, right: 70.0, left: 70.0),
+                        child: Text(
+                          'Discover your strengths with your mentor',
+                          style: AppFonts.medium(24),
+                          textAlign: TextAlign.center,
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              right: 70.0, left: 70.0, bottom: 40.0),
-                          child: Text(
-                            'Mentoo provides quality mentors to help with your career',
-                            style: AppFonts.regular(14),
-                            textAlign: TextAlign.center,
-                          ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            right: 70.0, left: 70.0, bottom: 40.0),
+                        child: Text(
+                          'Mentoo provides quality mentors to help with your career',
+                          style: AppFonts.regular(14),
+                          textAlign: TextAlign.center,
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 70.0),
-                          child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: AppColors.mPrimary,
-                                fixedSize: const Size(350, 70),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(100)),
-                              ),
-                              onPressed: () => {},
-                              child: Text('Get Started',
-                                  style: AppFonts.medium(25))),
-                        )
-                      ],
-                    ),
+                      ),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.mPrimary,
+                            fixedSize: const Size(350, 70),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(100)),
+                          ),
+                          onPressed: () => {},
+                          child: Text('Get Started',
+                              style: AppFonts.medium(25))),
+                      const SizedBox(height: 30,),
+                      SvgPicture.asset(
+                        'assets/images/powered.svg',
+                        width: MediaQuery.of(context).size.width / 2.5,
+                      ),
+                    ],
                   ),
                 ),
               ),
