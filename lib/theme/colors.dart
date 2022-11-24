031e14d4d833daf077a7696fa8eadf3d8a19bdfa
ThongHT <thonght150201@gmail.com>
2022-11-24 09:54:27 +0700

    add stuff

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -6,5 +6,8 @@ class AppColors {
   static Color m_background = const Color(0xffEADDFF);
   static Color mText = const Color(0xff1C1B1F);
   static Color mPrimary = const Color(0xffD0BCFF);
+  static Color mGray = const Color(0xff49454F);
+  static Color mDarkPurple = const Color(0xff381E72);
+  static Color mLightPurple = const Color(0xff9A82DB);
   const AppColors();
 }
